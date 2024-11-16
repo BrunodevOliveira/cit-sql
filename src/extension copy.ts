24ac88411d1bd9e3e1925b362024841a2650d781
Bruno Oliveira <brunodevoliveira@gmail.com>
2024-11-16 00:11:07 -0300

    Adicionado consulta com Endpoint e Token fixos

---
+++ src/extension copy.ts
@@ -0,0 +1,17 @@
+import * as vscode from 'vscode';
+
+export function activate(context: vscode.ExtensionContext) {
+	// The command has been defined in the package.json file
+	// Now provide the implementation of the command with registerCommand
+	// The commandId parameter must match the command field in package.json
+	const disposable = vscode.commands.registerCommand('cit-sql.helloWorld', () => {
+		// The code you place here will be executed every time your command is executed
+		// Display a message box to the user
+		vscode.window.showInformationMessage('cit-SQL nasceu!');
+	});
+
+	context.subscriptions.push(disposable);
+}
+
+// This method is called when your extension is deactivated
+export function deactivate() {}
